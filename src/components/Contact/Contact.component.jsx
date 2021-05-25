72b8d82c57fe4cadab9e620377a4d2a5d29ebd34
Thomas Syczyk <tomsycz@gmail.com>
2021-05-25 19:53:59 +0100

    fixed incorrect link

--- src/components/Contact/Contact.component.jsx
+++ src/components/Contact/Contact.component.jsx
@@ -27,7 +27,10 @@ const Contact = () => {
           </a>
         </div>
         <div className="contact-detail">
-          <a href="linkedin.com/in/tomasz-syczyk-424103159" className="github">
+          <a
+            href="https://linkedin.com/in/tomasz-syczyk-424103159"
+            className="github"
+          >
             <SiLinkedin className="icon" /> LinkedIn
           </a>
         </div>
